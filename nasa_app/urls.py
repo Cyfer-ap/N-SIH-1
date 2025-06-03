357006aa684f3f6591d90b8a23c681cf953572da
Cyfer-ap <Cyfer-ap@users.noreply.github.com>
2025-06-04 00:39:43 +0530

    apod feature updated: update after 12:00PM

--- nasa_app/urls.py
+++ nasa_app/urls.py
@@ -4,6 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),  # APOD homepage
     path('apod/', views.apod, name='apod'),  # Handles APOD view
+    path('mars/', views.mars_rover_view, name='mars_rover'),
+
 ]
 from django.conf import settings
 from django.conf.urls.static import static
